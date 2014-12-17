09b8dd97d52c9ba1664c1f2a0c2adee3d7c2acc9
Daniel Carbone <daniel.p.carbone@gmail.com>
2014-12-17 15:55:21 -0600

    Attempting to fix travis.ci testing

--- tests/DirectoryIteratorPlus/DirectoryIteratorPlusTest.php
+++ tests/DirectoryIteratorPlus/DirectoryIteratorPlusTest.php
@@ -1,6 +1,43 @@
 <?php
 
-require __DIR__.'/../misc/file-generator.php';
+if (!function_exists('generateRandomString'))
+{
+    /**
+     * @param int $length
+     * @return string
+     */
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
+}
+
+if (!is_dir(__DIR__.'/../misc/couple-of-files'))
+    mkdir(__DIR__.'/../misc/couple-of-files');
+if (!is_dir(__DIR__.'/../misc/empty-directory'))
+    mkdir(__DIR__.'/../misc/empty-directory');
+
+/*
+ * The directory tests/misc/couple-of-files should have come with this package.
+ * The below logic populates that directory with 10 files and 10 directories, giving a total of 11 files
+ * and 10 directories.
+ *
+ * It also creates a symlink to the couple-of-files directory for symlink unit testing.
+ */
+if (is_dir(__DIR__.'/../misc/couple-of-files') && count(glob(__DIR__.'/../misc/couple-of-files/*.txt', GLOB_NOSORT)) === 0)
+{
+    for ($i = 0; $i < 10; $i++)
+    {
+        file_put_contents(__DIR__.'/../misc/couple-of-files/single-file-'.$i.'.txt', generateRandomString(100));
+
+        mkdir(__DIR__.'/../misc/couple-of-files/directory-'.$i);
+    }
+}
+
 
 /**
  * Class DirectoryIteratorPlus
